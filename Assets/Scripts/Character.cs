619357feb20ff06aa667374006534d7a2de2b701
David Soto <urielsonic@gmail.com>
2018-01-31 08:57:32 -0600

    Subo terminado el Laboratorio 3. Gracias David Soto 17551

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Character : MonoBehaviour {
-
+    public Animator anim;
+    public AudioSource audio;
     Rigidbody2D rb2d;
     SpriteRenderer sr;
     public Camera cam;
@@ -14,6 +15,8 @@ public class Character : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        audio = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
@@ -22,6 +25,7 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move)); 
         if (move != 0) {
             rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
             cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
@@ -32,6 +36,7 @@ public class Character : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump")) {
             rb2d.AddForce(Vector2.up*jumpForce);
+            audio.Play();
         }
 
 	}
